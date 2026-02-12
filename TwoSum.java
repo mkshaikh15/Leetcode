2c4769865330cc3f92a8b2d069d406ca0eb2c6bc
Murtuza Shaikh <77818735+mkshaikh15@users.noreply.github.com>
2026-02-12 18:51:40 -0500

    Using HashMap for O(n) time complexity.

--- TwoSum.java
+++ TwoSum.java
@@ -1,5 +1,17 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+	public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> kvp = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (!kvp.containsKey(complement))
+                kvp.put(nums[i], i);
+            else
+                return new int[]{(int)kvp.get(complement), i};
+            
+        }
+         return new int[]{0, 0};
+    }
+    /*public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i <= nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
                 if (target-nums[i] == nums[j])
@@ -7,5 +19,5 @@ class Solution {
             }
         }
         return new int[]{0, 0};
-    }
+    }*/
 }
